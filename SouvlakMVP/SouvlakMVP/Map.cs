d99d0f497b0eb64c6e81b28ec511b2a1bbc4ae6a
Adrian Świrszcz <swirszczadrian@gmail.com>
2023-03-18 14:11:07 +0100

    backup before bigger changes

--- SouvlakMVP/SouvlakMVP/Map.cs
+++ SouvlakMVP/SouvlakMVP/Map.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Numerics;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
+
+using indexT = System.Int32;
+using distanceT = System.Single;    // no maidens?
+
+
 namespace SouvlakMVP;
+
 
 /// <summary>
 /// Class for storing city's map (aka. graph in which vertices = intersections and edges = roads)
@@ -16,15 +23,15 @@ internal class Map
     /// <summary>
     /// Struct for holding data of a single intersection.
     /// Field "position" holds data used to display intersection on screen.
-    /// Field "connections: stores all connections in form of tuples (targetID, distance).
+    /// Field "connections" stores all connections in form of tuples (targetID, distance).
     /// </summary>
     public struct Intersection
     {
         public Vector2 position;
-        public List<(int targetID, int distance)> connections;
+        public List<(indexT targetID, distanceT distance)> connections;
 
 
-        public Intersection(Vector2 position, List<(int targetID, int distance)> connections)
+        public Intersection(Vector2 position, List<(indexT targetID, distanceT distance)> connections)
         {
             this.position = position;
             this.connections = connections;
@@ -33,7 +40,7 @@ internal class Map
         public Intersection(Vector2 position)
         {
             this.position = position;
-            this.connections = new List<(int targetID, int distance)>();
+            this.connections = new List<(indexT targetID, distanceT distance)>();
         }
 
         public override string ToString()
@@ -68,11 +75,17 @@ internal class Map
         public static bool operator ==(Intersection i1, Intersection i2) => i1.position == i2.position;
         public static bool operator !=(Intersection i1, Intersection i2) => i1.position != i2.position;
 
-        public (int targetID, int distance) this[int id]
+        public (indexT targetID, distanceT distance) this[int id]
         {
             get { return this.connections[id];  }
             set { this.connections[id] = value; }
         } 
+    }
+
+    public class DuplicateIntersectionException : Exception
+    {
+        public DuplicateIntersectionException() : base("Duplicate intersections are not allowed!") { }
+        public DuplicateIntersectionException(string message) : base(message) { }
     }
 
 
@@ -106,39 +119,50 @@ internal class Map
 
     public void AddIntersection(Intersection intersection)
     {
-        // Check if intersection exists
-        int thisID = this.graph.IndexOf(intersection);
-        if (thisID < 0)
+        if (this.graph.Any(inter => inter.position == intersection.position))
         {
-            // If not add it at the end of list
-            thisID = this.graph.Count();      
+            throw new DuplicateIntersectionException();
         }
         else
         {
-            // If it does clear current connections, since we cant have duplicate intersections
-            foreach(Intersection inter in this.graph)
+            indexT thisID = this.graph.Count();
+            this.graph.Add(intersection);
+            foreach ((indexT targetID, distanceT distance) connection in intersection.connections)
+            {
+                this.graph[connection.targetID].connections.Add((thisID, connection.distance));
+            }
+        }
+    }
+
+    public void AddIntersection(Vector2 position, List<(indexT targetID, distanceT distance)> connections)
+    {
+        this.AddIntersection(new Intersection(position, connections));
+    }
+
+    public void UpdateIntersection(Intersection intersection)
+    {
+        indexT thisID = this.graph.IndexOf(intersection);
+        if (thisID < 0)
+        {
+            throw new IndexOutOfRangeException("Cannot update intersection that does not exist!");
+        }
+        else
+        {
+            foreach (Intersection inter in this.graph)
             {
                 inter.connections.RemoveAll(connection => connection.targetID == thisID);
             }
-
-        }
-        this.graph[thisID] = intersection;
-        foreach ((int targetID, int distance) connection in intersection.connections)
-        {
-            this.graph[connection.targetID].connections.Add((thisID, connection.distance));
+            this.graph[thisID] = intersection;
+            foreach ((int targetID, int distance) connection in intersection.connections)
+            {
+                this.graph[connection.targetID].connections.Add((thisID, connection.distance));
+            }
         }
     }
 
-    public void AddConnection(int firstID, int secondID, int distance) 
+    public void UpdateIntersection(Vector2 position, List<(indexT targetID, distanceT distance)> connections)
     {
-        // No checking for duplicates, we can have multiple connections between two intersections
-        this.graph[firstID].connections.Add((secondID, distance));
-        this.graph[secondID].connections.Add((firstID, distance));
-    }
-
-    public void RemoveIntersection(Intersection intersection)
-    {
-        throw new NotImplementedException();
+        this.UpdateIntersection(new Intersection(position, connections));
     }
 
     public void RemoveIntersection(Vector2 position)
@@ -146,19 +170,37 @@ internal class Map
         throw new NotImplementedException();
     }
 
-    public void RemoveConnection(int firstID, int secondID)
+    public void RemoveIntersection(Intersection intersection)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    public void AddConnection(indexT firstID, indexT secondID, distanceT distance) 
+    {
+        // No checking for duplicates, we can have multiple connections between two intersections
+        this.graph[firstID].connections.Add((secondID, distance));
+        this.graph[secondID].connections.Add((firstID, distance));
+    }
+
+    public void UpdateConnection(indexT firstID, indexT secondID, distanceT distance)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveConnection(indexT firstID, indexT secondID)
     {
         this.graph[firstID].connections.RemoveAll(connection => connection.targetID == secondID);
         this.graph[secondID].connections.RemoveAll(connection => connection.targetID == firstID);
     }
-
-    public Intersection this[int i]
+   
+    public Intersection this[indexT i]
     {
         get { return this.graph[i]; }
         set { this.AddIntersection(value); }
     }
 
-    public int this[int i, int j] 
+    public distanceT this[indexT i, indexT j] 
     { 
         get { return this.graph[i].connections[j].distance; }
         set { this.AddConnection(i, j, value); }
