b01500d73ae3dbc9dc6f98af33084ab2b7d6a73f
Adrian Świrszcz <61964930+swirszcz-adrian@users.noreply.github.com>
2023-03-18 20:04:58 +0100

    First implementation of Map; prepared other files (#2)
    * re-create a branch to accomodate for gitignore
    * small cosmetic upgrade
    * delete

---
+++ SouvlakMVP/SouvlakMVP/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Numerics;
+
+namespace SouvlakMVP;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Map map = new Map();
+        map.AddIntersection(new Map.Intersection(new Vector2(0, 0)));
+        map.AddIntersection(new Vector2(1, 0), new Map.Road[1] { new Map.Road(0, 1f) });
+        map.AddIntersection(new Vector2(1, 1), new Map.Road[2] { new Map.Road(0, 1.44f), new Map.Road(1, 1f) });
+        Console.WriteLine(map.ToString());
+    }
+}
