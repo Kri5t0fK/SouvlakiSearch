eaec85948f6cd0992df685e2e8af898063c101c4
Adrian Świrszcz <swirszczadrian@gmail.com>
2023-03-28 21:38:47 +0200

    Moved Genotype inside partial class GeneticAlgorithm

--- SouvlakMVP/SouvlakMVP/Genotype.cs
+++ SouvlakMVP/SouvlakMVP/Genotype.cs
@@ -3,75 +3,80 @@ using System.Collections.Generic;
 
 using genotypeSizeT = System.Int32;
 
+
 namespace SouvlakMVP;
 
-/// <summary>
-/// Class representing a single genotype of genetic algorithm.
-/// </summary>
-public class Genotype
+
+public partial class GeneticAlgorithm
 {
-    public class NonUniqueVerticesException : Exception
-    {
-        public NonUniqueVerticesException() : base("Duplicate edges with different weights are not allowed!") { }
-        public NonUniqueVerticesException(string message) : base(message) { }
-    }
-
-    public class NonEvenNumberOfVerticesException : Exception
-    {
-        public NonEvenNumberOfVerticesException() : base("Genotype must have EVEN (ont odd) number of vertices!") { }
-        public NonEvenNumberOfVerticesException(string message) : base(message) { }
-    }
-
     /// <summary>
-    /// Genotype: a list of even number of "odd vertices".
-    /// Odd vertices - vertices with odd number of neighbours.
+    /// Class representing a single genotype of genetic algorithm.
     /// </summary>
-    private readonly List<Graph.Vertex> _oddVertices;
-    public List<Graph.Vertex> OddVertices { get { return new List<Graph.Vertex>(this._oddVertices); } }
-    // Read-only Indexer for accesing the list
-    public Graph.Vertex this[int i]
+    public class Genotype
     {
-        get { return this._oddVertices[i]; }
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="_oddVertices"></param>
-    /// <exception cref="ArgumentException"></exception>
-    public Genotype(List<Graph.Vertex> _oddVertices)
-    {
-        // Validity checks
-        if(!(_oddVertices.Distinct().Count() == _oddVertices.Count()))
+        public class NonUniqueVerticesException : Exception
         {
-            throw new NonUniqueVerticesException();
-        }
-        if(!(_oddVertices.Count() % 2 == 0))
-        {
-            throw new NonEvenNumberOfVerticesException();
+            public NonUniqueVerticesException() : base("Duplicate edges with different weights are not allowed!") { }
+            public NonUniqueVerticesException(string message) : base(message) { }
         }
 
-        this._oddVertices = _oddVertices;
-    }
-
-    public List<(Graph.Vertex, Graph.Vertex)> GetPairs()
-    {
-        // Define output list
-        List<(Graph.Vertex,Graph.Vertex)> output = new();
-
-        // Get number of pairs in vertex list, list count is always even
-        int pairsCount = this.Size/2;
-        for(int pairID=0; pairID < pairsCount; pairID++)
+        public class NonEvenNumberOfVerticesException : Exception
         {
-            (Graph.Vertex, Graph.Vertex) pair = (this[pairID], this[pairID+1]); 
-            output.Add(pair);
+            public NonEvenNumberOfVerticesException() : base("Genotype must have EVEN (ont odd) number of vertices!") { }
+            public NonEvenNumberOfVerticesException(string message) : base(message) { }
         }
-        // Return list of pairs
-        return output;
-    }
 
-    public genotypeSizeT Size
-    {
-        get { return this._oddVertices.Count; }
+        /// <summary>
+        /// Genotype: a list of even number of "odd vertices".
+        /// Odd vertices - vertices with odd number of neighbours.
+        /// </summary>
+        private readonly List<Graph.Vertex> _oddVertices;
+        public List<Graph.Vertex> OddVertices { get { return new List<Graph.Vertex>(this._oddVertices); } }
+        // Read-only Indexer for accesing the list
+        public Graph.Vertex this[int i]
+        {
+            get { return this._oddVertices[i]; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="_oddVertices"></param>
+        /// <exception cref="ArgumentException"></exception>
+        public Genotype(List<Graph.Vertex> _oddVertices)
+        {
+            // Validity checks
+            if (!(_oddVertices.Distinct().Count() == _oddVertices.Count()))
+            {
+                throw new NonUniqueVerticesException();
+            }
+            if (!(_oddVertices.Count() % 2 == 0))
+            {
+                throw new NonEvenNumberOfVerticesException();
+            }
+
+            this._oddVertices = _oddVertices;
+        }
+
+        public List<(Graph.Vertex, Graph.Vertex)> GetPairs()
+        {
+            // Define output list
+            List<(Graph.Vertex, Graph.Vertex)> output = new();
+
+            // Get number of pairs in vertex list, list count is always even
+            int pairsCount = this.Size / 2;
+            for (int pairID = 0; pairID < pairsCount; pairID++)
+            {
+                (Graph.Vertex, Graph.Vertex) pair = (this[pairID], this[pairID + 1]);
+                output.Add(pair);
+            }
+            // Return list of pairs
+            return output;
+        }
+
+        public genotypeSizeT Size
+        {
+            get { return this._oddVertices.Count; }
+        }
     }
 }
