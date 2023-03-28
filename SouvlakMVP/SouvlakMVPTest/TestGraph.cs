71c26d7ac5a6b25f41522c10b237761f896e30bb
justynagibas <juztynagibas@gmail.com>
2023-03-28 10:18:06 +0200

    Unit Tests: New tests for Graph vertices.

--- SouvlakMVP/SouvlakMVPTest/TestGraph.cs
+++ SouvlakMVP/SouvlakMVPTest/TestGraph.cs
@@ -1,12 +1,14 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SouvlakMVP;
 using System.Numerics;
+using indexT = System.Int32;
 
 namespace SouvlakMVPTest
 {
     [TestClass]
     public class TestGraph
     {
+
         [TestMethod]
         public void GetVertexCount_zeroVertices()
         {
@@ -71,5 +73,97 @@ namespace SouvlakMVPTest
             Vector2 position = new Vector2(0, -5);
             Assert.AreNotEqual(testGraph.ContainsVertex(position), true);
         }
+
+        [TestMethod]
+        public void GetVertexIdx_withVertexInGraph()
+        {
+            Graph.Vertex vertex = new Graph.Vertex(new Vector2(1, 1));
+            Graph testGraph = new Graph(vertex);
+            indexT vertex_idx = testGraph.GetVertexIdx(vertex);
+
+
+            indexT expected = 0;
+
+            Assert.AreEqual(expected, vertex_idx);
+        }
+
+        [TestMethod]
+        public void GetVertexIdx_withoutVertexInGraph()
+        {
+            // in my opinion it should throw exception 
+            Graph testGraph = new Graph();
+            indexT vertex_id = testGraph.GetVertexIdx(new Graph.Vertex(new Vector2(1, 1)));
+
+            indexT expected = -1;
+
+            Assert.AreEqual(expected, vertex_id);
+
+        }
+
+        [TestMethod]
+        public void GetVertexIdxByPosition_withVertexInGraph()
+        {
+            Graph.Vertex[] vertices_position = { new Graph.Vertex(new Vector2(1, 1)),
+                                                     new Graph.Vertex(new Vector2(2, 2))};
+            List<Graph.Vertex> vertices = new List<Graph.Vertex>(vertices_position);
+            Graph testGraph = new Graph(vertices);
+
+            indexT vertex_idx = testGraph.GetVertexIdx(new Vector2(2, 2));
+
+            indexT expected = 1;
+
+            Assert.AreEqual(expected, vertex_idx);
+
+        }
+
+        public void GetVertexIdxByPosition_withoutVertexInGraph()
+        {
+            // in my opinion it should throw exception 
+            Graph testGraph = new Graph();
+            indexT vertex_id = testGraph.GetVertexIdx(new Vector2(1, 1));
+
+            indexT expected = -1;
+
+            Assert.AreEqual(expected, vertex_id);
+
+        }
+
+        [TestMethod]
+        public void ToString_EmptyGraph()
+        {
+            Graph graph = new Graph();
+            string expected = string.Empty;
+
+            Assert.AreEqual(expected, graph.ToString());
+
+        }
+
+        /* One convention to convert to string does not exist
+        [TestMethod]
+        public void ToString_OnlyVerticesinGraph()
+        {
+            Graph.Vertex[] vertices_position = { new Graph.Vertex(new Vector2(1, 1)), new Graph.Vertex(new Vector2(2, 2))};
+            List<Graph.Vertex> vertices = new List<Graph.Vertex>(vertices_position);
+            Graph graph = new Graph(vertices);
+            string expected = "<1  1> : []\n<2  2> : []\n";
+
+            Assert.AreEqual(expected, graph.ToString());
+
+        }
+        */
+
+        [TestMethod]
+        public void ToString_GraphwithEdges()
+        {
+            Graph.Vertex[] vertices_position = { new Graph.Vertex(new Vector2(1, 1), new Graph.Edge[1] { new Graph.Edge(1, 2f) }),
+                                                     new Graph.Vertex(new Vector2(2, 2), new Graph.Edge[1] { new Graph.Edge(0, 3f) })};
+            List<Graph.Vertex> vertices = new List<Graph.Vertex>(vertices_position);
+            Graph graph = new Graph(vertices);
+            string expected = "<1  1> : [1 : 1 x 2,00]\n<2  2> : [0 : 1 x 3,00]\n";
+
+            Assert.AreEqual(expected, graph.ToString());
+
+        }
+
     }
 }
