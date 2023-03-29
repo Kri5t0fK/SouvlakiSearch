4785fb0b75dc96f80eb898952e1a90c6a8a4de2e
Jan Pomykacz <jasiek2miliony@gmail.com>
2023-03-29 17:59:04 +0200

    Implemented initialization

--- SouvlakMVP/SouvlakMVP/Generation.cs
+++ SouvlakMVP/SouvlakMVP/Generation.cs
@@ -6,7 +6,6 @@ using System.Text;
 using System.Threading.Tasks;
 using static SouvlakMVP.Genotype;
 
-
 namespace SouvlakMVP;
 
 
@@ -17,13 +16,24 @@ public partial class GeneticAlgorithm
     /// </summary>
     public class Generation
     {
+        // field rng is used for shuffling in generating population
+        private static Random rng = new Random();
         private int populationSize;
         private Genotype[] population;
-        public Generation(int size)
-        {
-            this.populationSize = size;
-            this.population = new Genotype[populationSize];
 
+        /// <exception cref="ArgumentException"></exception>
+        public Generation(List<Graph.Vertex> init_population, int size)
+        {
+            if(size > 0)
+            {
+                this.populationSize = size;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Population size can't be negative!");
+            }
+            this.population = new Genotype[populationSize];
+            GeneratePopulation(init_population, this.populationSize);
         }
 
         public Generation(Genotype[] population)
@@ -33,9 +43,28 @@ public partial class GeneticAlgorithm
             this.population = population;
         }
 
-        public void GeneratePopulation()
+        /// <summary>
+        /// Method used in Generator constructor. Used for creating population.
+        /// </summary>
+        /// <param name="init_population">Initial genotype (list of Vertex) used for creating population.</param>
+        /// <param name="population_size">Specifies amount of subsets created in current generation.</param>
+        private void GeneratePopulation(List<Graph.Vertex> init_population, int population_size)
         {
-            //     @TODO  Implement Initialization
+            // loop for creating population_size numbers of genotypes in population
+            for(uint genotypeIdx = 0; genotypeIdx < population_size; genotypeIdx++)
+            {
+                // Fisher-Yates algorithm for shuffling genotype members
+                List<Graph.Vertex> curr_genotype = new List<Graph.Vertex>(init_population);
+                int no_of_vertices = curr_genotype.Count;
+                while (no_of_vertices > 1)
+                {
+                    int rand_vertex = Generation.rng.Next(no_of_vertices--);
+                    Graph.Vertex value = curr_genotype[rand_vertex];
+                    curr_genotype[rand_vertex] = curr_genotype[no_of_vertices];
+                    curr_genotype[no_of_vertices] = value;
+                }
+                this.population[genotypeIdx] = new Genotype(curr_genotype);
+            }
         }
     }
 }
---
+++ SouvlakMVP/SouvlakMVPTest/TestGeneration.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SouvlakMVP;
+using System.Numerics;
+
+namespace SouvlakMVPTest
+{
+    [TestClass]
+    public class TestGeneration
+    {
+        [TestMethod]
+        public void GenerationConstructor_ExceptionThrow()
+        {
+            // Example map (graph) with 6 intersections (vertices) 
+            Graph graph = new Graph();
+            // Intersections from 0 to 5 
+            graph.AddVertex(new Graph.Vertex(new Vector2(0, 0)));
+            graph.AddVertex(new Graph.Vertex(new Vector2(1, 0)));
+            graph.AddVertex(new Graph.Vertex(new Vector2(0, 1)));
+            graph.AddVertex(new Graph.Vertex(new Vector2(1, 1)));
+            graph.AddVertex(new Graph.Vertex(new Vector2(2, 1)));
+            graph.AddVertex(new Graph.Vertex(new Vector2(1, 2)));
+            // 9 roads and their distances 
+            graph.AddEdge(0, 1, 3f);
+            graph.AddEdge(0, 5, 6f);
+            graph.AddEdge(0, 4, 3f);
+            graph.AddEdge(1, 2, 1f);
+            graph.AddEdge(1, 3, 3f);
+            graph.AddEdge(2, 3, 3f);
+            graph.AddEdge(2, 5, 1f);
+            graph.AddEdge(3, 5, 1f);
+            graph.AddEdge(4, 5, 2f);
+
+            List<Graph.Vertex> vertices = graph.GetUnevenVertices();
+
+            Action badPopulationSize = () => new GeneticAlgorithm.Generation(vertices, -4);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(badPopulationSize);
+        }
+
+        [TestMethod]
+        public void GenerationConstructor_CreatingObject()
+        {
+            // Example map (graph) with 6 intersections (vertices) 
+            Graph graph = new Graph();
+            // Intersections from 0 to 5 
+            graph.AddVertex(new Graph.Vertex(new Vector2(0, 0)));
+            graph.AddVertex(new Graph.Vertex(new Vector2(1, 0)));
+            graph.AddVertex(new Graph.Vertex(new Vector2(0, 1)));
+            graph.AddVertex(new Graph.Vertex(new Vector2(1, 1)));
+            graph.AddVertex(new Graph.Vertex(new Vector2(2, 1)));
+            graph.AddVertex(new Graph.Vertex(new Vector2(1, 2)));
+            // 9 roads and their distances 
+            graph.AddEdge(0, 1, 3f);
+            graph.AddEdge(0, 5, 6f);
+            graph.AddEdge(0, 4, 3f);
+            graph.AddEdge(1, 2, 1f);
+            graph.AddEdge(1, 3, 3f);
+            graph.AddEdge(2, 3, 3f);
+            graph.AddEdge(2, 5, 1f);
+            graph.AddEdge(3, 5, 1f);
+            graph.AddEdge(4, 5, 2f);
+
+            List<Graph.Vertex> vertices = graph.GetUnevenVertices();
+            GeneticAlgorithm.Generation testgen = new GeneticAlgorithm.Generation(vertices, 6);
+            Assert.IsInstanceOfType(testgen, typeof(GeneticAlgorithm.Generation));
+        }
+    }
+}
