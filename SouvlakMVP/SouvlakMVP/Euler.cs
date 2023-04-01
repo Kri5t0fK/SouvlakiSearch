717dfbdb9ea4560a10cbeaff1e3679cd030ba376
Michał Święciło <michal_00-00@o2.pl>
2023-04-01 14:00:09 +0200

    New Euler class

---
+++ SouvlakMVP/SouvlakMVP/Euler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using indexT = System.Int32;
+using edgeWeightT = System.Single;
+using System.Runtime.InteropServices;
+
+namespace SouvlakMVP;
+
+public class Euler
+{
+    /// <summary>Finds an Eulerian cycle in the given undirected graph using Hierholzer's algorithm.</summary>
+    /// <param name="graph">The graph to search.</param>
+    /// <param name="startVertex">Optional starting vertex.</param>
+    /// <returns>The Eulerian cycle as a list of vertices.</returns>
+    public static List<indexT> FindEulerPath(Graph graph, indexT? startVertex = null)
+    {
+
+    }
+}
