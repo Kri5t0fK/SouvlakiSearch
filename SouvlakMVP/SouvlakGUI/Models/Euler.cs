15965e7f5cd7bcefb5974fa69f6282b66e7fb6ee
Adrian Świrszcz <swirszczadrian@gmail.com>
2023-04-13 15:22:04 +0200

    necessary changes to source files, to make them work in new project

---
+++ SouvlakMVP/SouvlakGUI/Models/Euler.cs
@@ -0,0 +1,115 @@
+﻿using indexT = System.Int32;
+using edgeWeightT = System.Single;
+using System.Data;
+
+namespace SouvlakGUI.Models;
+
+
+public class Euler
+{
+    /// <summary>Finds an Eulerian cycle in the given undirected graph using Hierholzer's algorithm.</summary>
+    /// <param name="graph">The graph to search.</param>
+    /// <param name="startVertexP">Optional starting vertex.</param>
+    /// <returns>Tuple with the Eulerian cycle as a list of vertices and total cost.</returns>
+    public static (List<indexT>, edgeWeightT) FindEulerCycle(Graph graph, indexT? startVertexP = null, bool copyGraph = true)
+    {
+        // If aksed, operate on copy of graph
+        if (copyGraph)
+        {
+            graph = graph.DeepCopy();
+        }
+        graph = graph.DeepCopy();
+        indexT startVertex = 0;
+
+        // Check the correctness of the input parameter
+        if (!HasEulerCycle(graph))
+        {
+            throw new DataException("Graph does not have an euler cycle.");
+        }
+
+        if (startVertexP != null)
+        {
+            indexT tempID = startVertexP.Value;
+            if (tempID >= 0 && tempID < graph.GetVertexCount())
+            {
+                startVertex = tempID;
+            }
+            else
+            {
+                throw new ArgumentException($"There is no vertex with ID = {tempID} in the graph.");
+            }
+        }
+
+        // Stack of vertices with non-zero degree
+        Stack<indexT> nonIsolatedVerticesID = new Stack<indexT>(new[] { startVertex });
+
+        List<indexT> eulerCycle = new List<indexT>();
+        edgeWeightT totalCost = 0;
+
+        while (nonIsolatedVerticesID.Count > 0)
+        {
+            // Get the vertexID from top of the stack (without removing)
+            indexT tempVertexID = nonIsolatedVerticesID.Peek();
+            // Get the list of the edges coming out from the vertex
+            List<Edge> edgesFromVertex = graph[tempVertexID].edgeList;
+
+            if (edgesFromVertex.Count == 0)
+            {
+                // Add vertexID to final path and delate it from stack
+                eulerCycle.Add(tempVertexID);
+                nonIsolatedVerticesID.Pop();
+            }
+            else
+            {
+                // Find any edge coming out of vertex, add its target to stack and remove it from graph
+                Edge edge = edgesFromVertex[0];
+                nonIsolatedVerticesID.Push(edge.targetIdx);
+                totalCost += edge.weight;
+                graph.RemoveEdge(tempVertexID, edge.targetIdx);
+            }
+        }
+
+        return (eulerCycle, totalCost);
+    }
+
+    /// <summary>Checks if the given undirected graph contains an Euler cycle.</summary>
+    /// <param name="graph">The graph to search.</param>
+    /// <returns>True if the graph contains an Euler cycle, false otherwise.</returns>
+    public static bool HasEulerCycle(Graph graph)
+    {
+        int verticesN = graph.GetVertexCount();
+
+        for (int i = 0; i < verticesN; i++)
+        {
+            int edgesN = graph[i].GetEdgeCount();
+            if (edgesN == 0 || (edgesN % 2) != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /// <summary>Checks if the given undirected graph contains an Euler path.</summary>
+    /// <param name="graph">The graph to search.</param>
+    /// <returns>True if the graph contains an Euler path, false otherwise.</returns>
+    public static bool HasEulerPath(Graph graph)
+    {
+        int verticesN = graph.GetVertexCount();
+        int oddVerticesCounter = 0;
+
+        for (int i = 0; i < verticesN; i++)
+        {
+            int edgesN = graph[i].GetEdgeCount();
+            if (edgesN == 0)
+            {
+                return false;
+            }
+            else if ((edgesN % 2) != 0 && ++oddVerticesCounter > 2)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ SouvlakMVP/SouvlakGUI/Models/Vector2Converter.cs
@@ -0,0 +1,77 @@
+﻿using System.Numerics;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace SouvlakGUI.Models;
+
+
+/// <summary>
+/// Provides custom JSON serialization and deserialization for Vector2 objects using the System.Text.Json namespace.
+/// </summary>
+public class Vector2Converter : JsonConverter<Vector2>
+{
+    /// <summary>
+    /// Reads JSON and converts it into a Vector2 object.
+    /// </summary>
+    /// <param name="reader">The reader used to parse the JSON data.</param>
+    /// <param name="typeToConvert">The type of object to convert.</param>
+    /// <param name="options">The serializer options.</param>
+    /// <returns>A Vector2 object deserialized from the JSON data.</returns>
+    public override Vector2 Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType != JsonTokenType.StartObject)
+        {
+            throw new JsonException($"Unexpected token type '{reader.TokenType}'");
+        }
+
+        float x = 0, y = 0;
+
+        while (reader.Read())
+        {
+            if (reader.TokenType == JsonTokenType.EndObject)
+            {
+                return new Vector2(x, y);
+            }
+
+            if (reader.TokenType != JsonTokenType.PropertyName)
+            {
+                throw new JsonException($"Unexpected token type '{reader.TokenType}'");
+            }
+
+            string propertyName = reader.GetString();
+
+            if (!reader.Read())
+            {
+                throw new JsonException($"Unexpected end of JSON object");
+            }
+
+            switch (propertyName)
+            {
+                case "X":
+                    x = reader.GetSingle();
+                    break;
+                case "Y":
+                    y = reader.GetSingle();
+                    break;
+                default:
+                    throw new JsonException($"Unknown property '{propertyName}'");
+            }
+        }
+
+        throw new JsonException("Unexpected end of JSON input");
+    }
+
+    /// <summary>
+    /// Writes a Vector2 object to JSON.
+    /// </summary>
+    /// <param name="writer">The writer used to write the JSON data.</param>
+    /// <param name="value">The Vector2 object to serialize.</param>
+    /// <param name="options">The serializer options.</param>
+    public override void Write(Utf8JsonWriter writer, Vector2 value, JsonSerializerOptions options)
+    {
+        writer.WriteStartObject();
+        writer.WriteNumber("X", value.X);
+        writer.WriteNumber("Y", value.Y);
+        writer.WriteEndObject();
+    }
+}
